62781304f045bcb91f54d1d39c5af873588f3d4d
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-03-29 13:07:53 +0530

    backend: mock data frontend: header footer

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'John Players Blue Jeans',
+    image: '/images/jeans.webp',
+    description:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ',
+    brand: 'John Players',
+    category: 'Jeans',
+    price: 4399,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'CNY Logo Print Polo T-shirt',
+    image: '/images/tshirt.webp',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages',
+    brand: 'Super Dry',
+    category: 'T-Shirts',
+    price: 3999,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: 'Code Quilted Slim Fit Puffer Jacket',
+    image: '/images/jacket.webp',
+    description:
+      'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont',
+    brand: 'Super Dry',
+    category: 'Jackets',
+    price: 4339,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Brand Print Round-Neck Sweatshirt',
+    image: '/images/sweatshirt.jpeg',
+    description:
+      'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,',
+    brand: 'U.S. POLO ASSN.',
+    category: 'Sweat Shirts',
+    price: 3999,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Concave Run IDP Colourblock Running Shoes',
+    image: '/images/shoes.webp',
+    description:
+      'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful',
+    brand: 'PUMA',
+    category: 'Shoes',
+    price: 1999,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    _id: '6',
+    name: 'Spirit Icon Hooded Zip-Front Puffer Jacket',
+    image: '/images/jacket2.webp',
+    description:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    brand: 'John Players',
+    category: 'Jackets',
+    price: 2999,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+]
+
+export default products
