52cf9612215edd079c342ccf540ea1b13c168487
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-04-06 09:54:06 +0530

    7.1 - db, server, errorMiddleware

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://chandu:chandu888@cluster0.ucbna.mongodb.net/test', {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
@@ -11,7 +11,7 @@ const connectDB = async () => {
         console.log(`MongoDB Connected! with ${conn.connection.host}`.cyan.underline)
     } catch (error) {
         console.error(`Error: ${error.message}`.red.underline.bold)
-
+        process.exit(1)
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import productRoutes from './routes/productRoutes.js'
+import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-connectDB()
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -15,14 +17,15 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
-app.use('/api/products/:id', productRoutes)
+
 app.use(notFound)
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
