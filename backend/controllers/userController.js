3ef67c34742f15c0b1fbcd1742bb55ba3a3a7f65
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-04-07 16:17:58 +0530

    9.1 - backend user auth, register and profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler'
-import User from '../models/userModel'
+import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 // @desc Auth user & get the token
@@ -25,4 +25,55 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
-export { authUser }
+// @desc Register user
+// @route POST /api/users/
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if(userExists) {
+      res.status(400)
+      throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+      name, email, password
+    })
+
+    if(user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id)
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
+})
+
+// @desc Profile route
+// @route GET /api/users/profile
+// @access private
+const getUserProfile = asyncHandler( async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,14 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
-import { authUser } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile } from '../controllers/userController.js'
 
 import { protect } from '../middleware/authMiddleware.js'
 
-// router.route('/').post(registerUser)
+router.route('/').post(registerUser) // default route is /api/users
 
 router.route('/login').post(authUser)
 
-// router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
