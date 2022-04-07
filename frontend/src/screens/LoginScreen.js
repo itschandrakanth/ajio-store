6799f81b297b01e41790af0b48281535f1277898
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-04-07 17:42:32 +0530

    9.2 - react login - reducers - actions - store

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+
+import { login } from '../actions/userActions'
+import FormContainer from '../components/FormContainer'
+
+function LoginScreen({ history, location }) {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector((state) => state.userLogin)
+
+    const { loading, userInfo, error } = userLogin
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    useEffect(() => {
+        if(userInfo) {
+            history.push(redirect)
+        }
+    },[history, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+    }
+  return (
+    <FormContainer>
+        <h1>Sign in</h1>
+        {error && <Message variant='danger'>{error}</Message>}
+
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+            <Form.Group>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" value={email}onChange={(e) => setEmail(e.target.value)}></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>Sign in</Button>
+        </Form>
+
+        <Row>
+            <Col>
+             New User?{' '}
+             <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+            </Col>
+        </Row>
+
+
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
