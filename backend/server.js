ae46177f7fb62913a791949d4e07ceeeec7cdd21
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-04-18 17:28:31 +0530

    14 - payment sdk, orderScreen - actions, reducers, store

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_RESET } from "../constants/orderConstants"
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_RESET, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL } from "../constants/orderConstants"
 
 export const orderCreateReducer = (state={}, action) => {
     switch (action.type) {
@@ -28,5 +28,18 @@ export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {
             return {}    
         default:
             return state            
+    }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_PAY_REQUEST:
+            return { loading: true }
+        case ORDER_PAY_SUCCESS:
+            return { loading: false, success: true }
+        case ORDER_PAY_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state    
     }
 }
